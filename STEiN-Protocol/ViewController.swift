072ee7352873463cdef4c519058b063cc499a3ec
Gregory Rudolph <gregory@stein13.net>
2015-06-15 11:50:16 -0400

    Initial Commit

---
+++ STEiN-Protocol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  STEiN-Protocol
+//
+//  Created by Gregory Rudolph on 6/15/15.
+//  Copyright (c) 2015 STEIN NET. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
